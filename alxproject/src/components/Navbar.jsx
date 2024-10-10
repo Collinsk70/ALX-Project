6eb7ef0248f6ab41ab049a2851a87c743eb9b49f
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 <collinskarweru@gmail.com>
2024-10-10 19:56:27 +0300

    commit

---
+++ alxproject/src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function Navbar() {
+  return (
+    <nav className="navbar">
+    <div className="logo">Envision Cook Enjoy</div>
+    <ul className="nav-links">
+      <li>Home</li>
+      <li>About</li>
+      <li>Search</li>
+    </ul>
+  </nav>
+  )
+}
